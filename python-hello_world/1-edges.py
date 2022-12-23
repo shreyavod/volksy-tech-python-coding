83de66b1e6dee2bb6723ce41b2701f2d8b8dd571
shreyavod <shreyav@brightcomgroup.com>
2022-12-23 11:04:45 +0000

    task1

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = [0-2]
-word-last_2 = [6]
+word_last_2 = [6]
 middle_word = [3-6]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
